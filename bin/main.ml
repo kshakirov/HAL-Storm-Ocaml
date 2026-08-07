0dc6b6e91744ab16c9c601bed40f7b345d5c4dea
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-08-07 13:52:13 +0300

    issue-1 ref #1 parser wip2

--- bin/main.ml
+++ bin/main.ml
@@ -5,8 +5,8 @@ let () =
   Eio_main.run @@ fun env ->
                   let request_line = "GET /api/v1/users HTTP/1.1\r\n" in
 
-                  let idx, s  = Parser.test_wirth request_line 0 ReqParse  in 
-                  traceln "%d" idx 
+                  let  s  = Parser.test_wirth request_line 0 {state=ReqParse; offsets=[0] }  in 
+                  traceln "%d" (List.nth s.offsets 0)
 
 
 
--- lib/parser.ml
+++ lib/parser.ml
@@ -1,28 +1,49 @@
 (* Состояния ДКА по Вирту для разбора первой строки HTTP *)
 type http_state =
-  | PanrseMethod     (* Ищем метод: GET, POST... *)
+  | ParseMethod     (* Ищем метод: GET, POST... *)
   | ParseUri        (* Ищем путь: /api/v1/users... *)
   | ParseVersion    (* Ищем версию: HTTP/1.1 *)
 
 type http_status =
   |ReqParse
   |ReqMethod
-  |ReqUriStart
   |ReqUri
+  |ExpectCRLF
+  |Error
+  |Success
   |ReqVersion[@@deriving show]
+
 
 
 let  test_hello n  =
   n
 
+type parserState = {state : http_status ; offsets: int list}
 
-let rec test_wirth str idx s =
+let check_method_3 str  =
+  match  String.capitalize_ascii(String.sub str 0 2)  with
+  |"get" -> true
+  |"put" -> true
+  |_-> false
+  
+
+let rec test_wirth str idx s:parserState =
+  Printf.printf  "test_wirth: idx = %d\n" idx;
   (* print_int idx; *)
-  match idx, s with
-  | 0, _ when String.get str 0 = 'G'  ->  test_wirth str (idx +1) ReqMethod
-  | 2, _  when String.get str 2 = 'T'  -> test_wirth str (idx +1) ReqUriStart
-  | _, ReqUriStart when String.get str  idx = ' ' -> test_wirth str (idx + 1)  ReqVersion
-  | _ -> (idx, s )
+  match (String.get str idx), s.state with
+  |_, ReqParse ->   test_wirth str (idx + 1) {state=ReqMethod;offsets=s.offsets}
+  | x, ReqMethod when x!= ' '  -> test_wirth str (idx + 1) {state=ReqUri;offsets=s.offsets}
+  | ' ', ReqMethod when idx= 3 &&  check_method_3 str    -> test_wirth str (idx + 1) {state=ReqUri;offsets=List.cons 4 (List.cons 2 s.offsets)}
+  | ' ', ReqMethod when idx = 3 &&   not (check_method_3 str)    -> {state= Error; offsets=[-1]}
+  | _, ReqUri when String.get str  idx  != ' ' -> test_wirth str (idx + 1)  {state=ReqUri;offsets=s.offsets}
+  | ' ',ReqUri -> test_wirth str  (idx + 1)  {state=ReqVersion;offsets=List.cons (idx + 1) (List.cons (idx -1) s.offsets)}
+  |x, ReqVersion when x  != '\r' -> test_wirth str (idx + 1)  {state=ReqVersion;offsets=s.offsets}
+  | '\r',ReqUri -> test_wirth str  (idx + 1)  {state=ExpectCRLF;offsets=List.cons (idx - 1)  s.offsets}
+  | '\n',ExpectCRLF -> test_wirth str  (idx + 1)  {state=Success;offsets=s.offsets}
+  | x,ExpectCRLF  when x != '\n'    -> {state= Error; offsets=[-1]}
+  | _ ->
+     Printf.printf "Successfully finishing parsing ";
+     {state= Success; offsets=s.offsets}
 
 
 
