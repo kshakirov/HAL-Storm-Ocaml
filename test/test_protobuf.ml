f5c1736fc75cd7bfc1a61d4852db60b876639c15
kshakirov <kirill.shakirov4@gmai.com>
2026-09-18 13:44:28 +0300

    started protubuf

---
+++ test/test_protobuf.ml
@@ -0,0 +1,48 @@
+open Eio.Std
+open Eio.Buf_read
+
+open List
+
+
+let parse_protobuf seq = seq 
+  
+
+let  frame1 = "\x08\x01"
+
+type proto_parser_state =
+  |Tag
+  |VarInt
+  |Payload
+
+
+let add_little_endian b = b (*just stub*)
+
+let rec parse_varint (t:Cstruct.t) values_tuple iv =
+  match Cstruct.length t with
+  |0 -> (t, values_tuple, iv)
+  |_ ->
+    let b = Cstruct.get_byte t 0 in 
+    match  b  with
+    | x when  x land 0x7f = 0  -> (Cstruct.sub t 1 (Cstruct.length t - 1), values_tuple, iv)
+    | x when  x land 0x7f = 1  -> parse_varint (Cstruct.sub t 1 (Cstruct.length t - 1))  values_tuple iv (*do something with h*)
+    | _ ->  (t,values_tuple,iv) (* just stub*)
+     
+     
+
+let parse_protobuf seq  values_tuple intermediate_value  =
+  match Cstruct.length seq with
+  |0 -> (seq, values_tuple, intermediate_value)
+  |_ ->
+    let b = Cstruct.get_byte seq 0 in 
+    match b with
+    |x when  x land 7 = 0 -> parse_varint (Cstruct.sub seq 1 (Cstruct.length seq - 1)) values_tuple ()
+    |_ -> (seq, values_tuple, intermediate_value)
+      
+  
+
+
+
+let () =
+  (* Обязательно заворачиваем вn Eio_main, чтобы работал Eio.Buf_read *)
+  Eio_main.run @@ fun _env ->
+                  Printf.printf "Working with Protobufs"
